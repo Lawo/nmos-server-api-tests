4d598603273584e4de265dacbd7f7e62134964da
LAWOheinigu1 <ulrich.heiniger@lawo.com>
2016-11-23 12:06:20 +0100

    Add tslint
    References #6553

--- test/rootHooks.ts
+++ test/rootHooks.ts
@@ -1,4 +1,7 @@
-// You may also pick any file and add “root”-level hooks. For example, add beforeEach() outside of all describe() blocks. This will cause the callback to beforeEach() to run before any test case, regardless of the file it lives in (this is because Mocha has an implied describe() block, called the “root suite”).
+// You may also pick any file and add “root”-level hooks. For example, add beforeEach()
+// outside of all describe() blocks. This will cause the callback to beforeEach() to run 
+// before any test case, regardless of the file it lives in (this is because Mocha has an 
+// implied describe() block, called the “root suite”).
 
 beforeEach(() => {
   // console.log('before every test in every file');
