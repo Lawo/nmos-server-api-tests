cfdd7be5bb52e87c720c98f9a655fd1dadbc2624
LAWOheinigu1 <ulrich.heiniger@lawo.com>
2016-11-24 16:17:12 +0100

    Add possibility to specify host from commandline: npm run test --host=192.168.1.66
    If no host is specified the default host localhost is used
    
    References #6556

--- test/url.ts
+++ test/url.ts
@@ -1,20 +1,20 @@
 export class Url {
-    private static host = 'http://localhost:';
+    private static host = process.env.npm_config_host || 'http://localhost';
     // if no port is passed as command line argument default port 15631 is used
     private static port = process.env.npm_config_port || '15631';
 
     public static get Node(): string {
-        let node = this.host + this.port + '/x-nmos/node/v1.0';
+        let node = this.host + ':' + this.port + '/x-nmos/node/v1.0';
         return node;
     }
 
     public static get Query(): string {
-        let query = this.host + this.port + '/x-nmos/query/v1.0';
+        let query = this.host + ':' + this.port + '/x-nmos/query/v1.0';
         return query;
     }
 
     public static get Registration(): string {
-        let registration = this.host +  this.port + '/x-nmos/registration/v1.0';
+        let registration = this.host + ':' +  this.port + '/x-nmos/registration/v1.0';
         return registration;
     }
 }
