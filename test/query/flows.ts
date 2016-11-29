3c072ec504969980930089a7eddf9e52a5532c95
LAWOheinigu1 <ulrich.heiniger@lawo.com>
2016-11-29 11:50:36 +0100

    Add flows tests
    References #6556

--- test/query/flows.ts
+++ test/query/flows.ts
@@ -6,36 +6,25 @@ import chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 describe('Query', () => {
-  describe('Sources', () => {
+  describe('Flows', () => {
 
-    let testSource = {
-          'description': 'Camera 1',
-          'format': 'urn:x-nmos:format:video',
-          'tags': {
-            'SourceDeviceType': [
-              'UHD Camera'
-            ],
-            'host': [
-              'host1'
-            ],
-            'location': [
-              'Location 1'
-            ]
-          },
-          'caps': {},
-          'version': '1441724551:288670563',
-          'parents': <any>[],
-          'label': 'TestSource1',
-          'id': '042a4126-0208-443d-bda6-833ffc27ed51',
-          'device_id': '21a28338-fb2e-4df5-9b55-d58e6124bc9f'
+    let testFlow = {
+      'description': 'Off-air proxy',
+      'format': 'urn:x-nmos:format:video',
+      'tags': {},
+      'label': 'Off-air proxy',
+      'version': '1441724130:194944510',
+      'parents': <any>[],
+      'source_id': 'c7b1c809-84d4-427d-b6bb-c8397c66ce2b',
+      'id': '0c1f03d7-7e94-4b21-94d1-3ffbee8a0606'
     };
 
     // hooks
     before((done) => {
-      // add test source
+      // add test flow
       let body = {
-        'type': 'source',
-        'data': testSource
+        'type': 'flow',
+        'data': testFlow
       };
 
       chai.request(Url.Registration)
@@ -47,18 +36,18 @@ describe('Query', () => {
     });
 
     after((done) => {
-      // remove test source
+      // remove test flow
       chai.request(Url.Registration)
-        .del('/resource/sources/' + testSource.id)
+        .del('/resource/flows/' + testFlow.id)
         .end((err, res) => {
           done();
         });
     });
 
     // test cases
-    it('should list all sources on /sources GET', (done) => {
+    it('should list all flows on /flows GET', (done) => {
       chai.request(Url.Query)
-        .get('/sources')
+        .get('/flows')
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res).to.be.json;
@@ -68,7 +57,7 @@ describe('Query', () => {
 
     it('should have all required properties', (done) => {
       chai.request(Url.Query)
-        .get('/sources')
+        .get('/flows')
         .end((err, res) => {
           expect(res).to.have.status(200);
 
@@ -78,9 +67,8 @@ describe('Query', () => {
             expect(entry).to.have.property('label');
             expect(entry).to.have.property('description');
             expect(entry).to.have.property('format');
-            expect(entry).to.have.property('caps');
             expect(entry).to.have.property('tags');
-            expect(entry).to.have.property('device_id');
+            expect(entry).to.have.property('source_id');
             expect(entry).to.have.property('parents');
 
             expect(entry.id).to.be.a('string');
@@ -88,82 +76,81 @@ describe('Query', () => {
             expect(entry.label).to.be.a('string');
             expect(entry.description).to.be.a('string');
             expect(entry.format).to.be.a('string');
-            expect(entry.caps).to.be.an('object');
             expect(entry.tags).to.be.an('object');
-            expect(entry.device_id).to.be.a('string');
+            expect(entry.source_id).to.be.a('string');
             expect(entry.parents).to.be.an('array');
           });
           done();
         });
     });
 
-    it('should contain the test sources', (done) => {
+    it('should contain the test flows', (done) => {
       chai.request(Url.Query)
-        .get('/sources')
+        .get('/flows')
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body.some((entry: any) => entry.label === testSource.label)).to.be.true;
+          expect(res.body.some((entry: any) => entry.label === testFlow.label)).to.be.true;
           done();
         });
     });
 
-    it('should return the desired source by device_id', (done) => {
+    it('should return the desired flow by source_id', (done) => {
       chai.request(Url.Query)
-        .get('/sources')
-        .field('device_id', testSource.device_id)
+        .get('/flows')
+        .field('source_id', testFlow.source_id)
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body.some((entry: any) => entry.device_id === testSource.device_id)).to.be.true;
+          expect(res.body.some((entry: any) => entry.source_id === testFlow.source_id)).to.be.true;
           done();
         });
     });
 
-    it('should return the desired source by label', (done) => {
+    it('should return the desired flow by label', (done) => {
       chai.request(Url.Query)
-        .get('/sources')
-        .field('label', testSource.label)
+        .get('/flows')
+        .field('label', testFlow.label)
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body.some((entry: any) => entry.label === testSource.label)).to.be.true;
+          expect(res.body.some((entry: any) => entry.label === testFlow.label)).to.be.true;
           done();
         });
     });
 
-    it('should return the desired source by description', (done) => {
+    it('should return the desired flow by description', (done) => {
       chai.request(Url.Query)
-        .get('/sources')
-        .field('description', testSource.description)
+        .get('/flows')
+        .field('description', testFlow.description)
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body.some((entry: any) => entry.description === testSource.description)).to.be.true;
+          expect(res.body.some((entry: any) => entry.description === testFlow.description)).to.be.true;
           done();
         });
     });
 
-    it('should return the desired source by format', (done) => {
+    it('should return the desired flow by format', (done) => {
       chai.request(Url.Query)
-        .get('/sources')
-        .field('format', testSource.format)
+        .get('/flows')
+        .field('format', testFlow.format)
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body.some((entry: any) => entry.format === testSource.format)).to.be.true;
+          expect(res.body.some((entry: any) => entry.format === testFlow.format)).to.be.true;
           done();
         });
     });
 
-    it('should get a single source by id', (done) => {
+    it('should get a single flow by id', (done) => {
       chai.request(Url.Query)
-        .get('/sources/' + testSource.id)
+        .get('/flows/' + testFlow.id)
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body.label).to.equal(testSource.label);
+          expect(res.body.label).to.equal(testFlow.label);
           done();
         });
     });
 
-    it('should return an error when the requested source id does not exist', (done) => {
+    it('should return an error when the requested flow id does not exist', (done) => {
       chai.request(Url.Query)
-        .get('/sources/wrong id')
+        .get('/flows/wrong id')
         .end((err, res) => {
           expect(res).to.have.status(404);
           done();
@@ -172,3 +159,4 @@ describe('Query', () => {
 
   });
 });
+
