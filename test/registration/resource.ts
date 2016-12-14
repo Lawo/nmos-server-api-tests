0995797c10bcc16e6e8228be7c3553540b6ead77
LAWOheinigu1 <ulrich.heiniger@lawo.com>
2016-12-14 17:54:25 +0100

    Add registration resource tests
    References #6555

---
+++ test/registration/resource.ts
@@ -0,0 +1,73 @@
+import { Url } from './../util/url';
+import { RegistrationUtil } from './../util/registrationUtil';
+
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+describe('Registration', () => {
+  describe('Resource', () => {
+
+    let testNode = {
+      'version': '1441973902:879053935',
+      'hostname': 'TestNode1',
+      'label': 'TestNode1',
+      'href': 'http://172.29.80.65:12345/',
+      'services': [
+        {
+          'href': 'http://172.29.80.65:12345/x-manufacturer/pipelinemanager/',
+          'type': 'urn:x-manufacturer:service:pipelinemanager'
+        }
+      ],
+      'caps': {},
+      'id': '3b8be755-08ff-452b-b217-c9151eb21193'
+    };
+
+    describe('not registered', () => {
+      // hooks
+      afterEach((done) => {
+        RegistrationUtil.removeResource(done, Url.Registration, '/nodes', testNode.id);
+      });
+
+      // test cases
+      it('should create a registered resource', (done) => {
+        RegistrationUtil.createOrUpdateResource(done, Url.Registration, 'node', testNode, './specification/schemas/node.json', 201);
+      });
+
+      it('should fail when trying to get non existent resource (for debug use only)', (done) => {
+        RegistrationUtil.getResource(done, Url.Registration, '/nodes', testNode.id, 404);
+      });
+
+      it('should fail when trying to delete a non existent resource', (done) => {
+        RegistrationUtil.deleteResource(done, Url.Registration, '/nodes', testNode.id, 404);
+      });
+
+    });
+
+    describe('already registered', () => {
+      // hooks
+      beforeEach((done) => {
+        RegistrationUtil.addResource(done, Url.Registration, 'node', testNode);
+      });
+
+      afterEach((done) => {
+        RegistrationUtil.removeResource(done, Url.Registration, '/nodes', testNode.id);
+      });
+
+      // test cases
+      it('should update a registered resource', (done) => {
+        RegistrationUtil.createOrUpdateResource(done, Url.Registration, 'node', testNode, './specification/schemas/node.json', 200);
+      });
+
+      it('should get a registered resource (for debug use only)', (done) => {
+        RegistrationUtil.getResource(done, Url.Registration, '/nodes', testNode.id, 200);
+      });
+
+      it('should delete a registered resource', (done) => {
+        RegistrationUtil.deleteResource(done, Url.Registration, '/nodes', testNode.id, 204);
+      });
+
+    });
+
+  });
+});
---
+++ test/util/registrationUtil.ts
@@ -0,0 +1,72 @@
+import { expect } from 'chai';
+
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+import loadJsonFile = require('load-json-file');
+
+export class RegistrationUtil {
+
+  // hooks
+  public static addResource(done: MochaDone, url: string, type: string, testResource: Object) {
+    // add test resource
+    let body = {
+      'type': type,
+      'data': testResource
+    };
+
+    chai.request(url)
+      .post('/resource')
+      .send(body)
+      .end((err, res) => {
+        done();
+      });
+  }
+
+  public static removeResource(done: MochaDone, url: string, resourceType: string, id: string) {
+    // remove test resource
+    chai.request(url)
+      .del('/resource' + resourceType + '/' + id)
+      .end((err, res) => {
+        done();
+      });
+  }
+
+  // test cases
+  public static createOrUpdateResource(done: MochaDone, url: string, type: string, testResource: any, schemaFile: string, status: number) {
+    let body = {
+      'type': type,
+      'data': testResource
+    };
+
+    chai.request(url)
+      .post('/resource')
+      .send(body)
+      .end((err, res) => {
+        expect(res).to.have.status(status);
+        expect((<any>res).headers).property('location');
+        let schema = loadJsonFile.sync('./specification/schemas/node.json');
+        expect(res.body).to.be.jsonSchema(schema);
+        done();
+      });
+  }
+
+  public static getResource(done: MochaDone, url: string, resourceType: string, id: string, status: number) {
+    chai.request(url)
+      .get('/resource' + resourceType + '/' + id)
+      .end((err, res) => {
+        expect(res).to.have.status(status);
+        done();
+      });
+  }
+
+  public static deleteResource(done: MochaDone, url: string, resourceType: string, id: string, status: number) {
+    chai.request(url)
+      .del('/resource' + resourceType + '/' + id)
+      .end((err, res) => {
+        expect(res).to.have.status(status);
+        done();
+      });
+  }
+}
