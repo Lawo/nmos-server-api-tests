e30993e6462a60f0483b7cdce76435cd114637a6
LAWOheinigu1 <ulrich.heiniger@lawo.com>
2017-01-05 15:37:32 +0100

    Add possibility to specify separate ports for each of the APIs from commandline:
    npm run test --node_port=12345 --query_port=8870 --registration_port=8235
    
    References #6556

--- test/util/url.ts
+++ test/util/url.ts
@@ -1,20 +1,28 @@
 export class Url {
+  // if no host is passed as command line argument localhost is used
   private static host = process.env.npm_config_host || 'http://localhost';
-  // if no port is passed as command line argument default port 15631 is used
-  private static port = process.env.npm_config_port || '15631';
+
+  // if no nodeport is passed as command line argument default port 15631 is used
+  private static nodePort = process.env.npm_config_node_port || '15631';
+
+  // if no queryport is passed as command line argument default port 15631 is used
+  private static queryPort = process.env.npm_config_query_port || '15631';
+
+  // if no registrationport is passed as command line argument default port 15631 is used
+  private static registrationPort = process.env.npm_config_registration_port || '15631';
 
   public static get Node(): string {
-    let node = this.host + ':' + this.port + '/x-nmos/node/v1.0';
+    let node = this.host + ':' + this.nodePort + '/x-nmos/node/v1.0';
     return node;
   }
 
   public static get Query(): string {
-    let query = this.host + ':' + this.port + '/x-nmos/query/v1.0';
+    let query = this.host + ':' + this.queryPort + '/x-nmos/query/v1.0';
     return query;
   }
 
   public static get Registration(): string {
-    let registration = this.host + ':' + this.port + '/x-nmos/registration/v1.0';
+    let registration = this.host + ':' + this.registrationPort + '/x-nmos/registration/v1.0';
     return registration;
   }
 }
