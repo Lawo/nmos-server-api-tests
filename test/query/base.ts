6d0bb2c058598a3da18fd8eeefbd57af0c495021
heinigu1 <ulrich.heiniger@lawo.com>
2017-02-02 10:28:48 +0100

    Add schema verification to query base

--- test/query/base.ts
+++ test/query/base.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { JsonSchema } from './../util/jsonSchema';
 import { Url } from './../util/url';
 
 import * as chai from 'chai';
@@ -8,39 +9,34 @@ chai.use(chaiHttp);
 describe('Query', () => {
   describe('Base (/)', () => {
 
-    it('GET should return list of paths available from this API', (done) => {
-      chai.request(Url.Query)
-        .get('/')
-        .end(function (err, res) {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.include('nodes/');
-          expect(res.body).to.include('sources/');
-          expect(res.body).to.include('flows/');
-          expect(res.body).to.include('devices/');
-          expect(res.body).to.include('senders/');
-          expect(res.body).to.include('receivers/');
-          expect(res.body).to.include('subscriptions/');
-          done();
-        });
+    it('GET should return list of paths available from this API', () => {
+      return (async () => {
+        let res = await chai.request(Url.Query).get('/');
+
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+
+        let baseSchema = new JsonSchema('./specification/schemas', 'queryapi-base.json');
+        baseSchema.validate(res.body);
+      })();
     });
 
-    it('OPTIONS should return empty answer and Status OK.', (done) => {
-      chai.request(Url.Query)
-        .options('/')
-        .end(function (err, res) {
-          expect(res).to.have.status(200);
-          done();
-        });
+    it('OPTIONS should return empty answer and Status OK.', () => {
+      return (async () => {
+        let res = await chai.request(Url.Query).options('/');
+        expect(res).to.have.status(200);
+      })();
+
     });
 
-    it('POST should not be supported.', (done) => {
-      chai.request(Url.Query)
-        .post('/')
-        .end(function (err, res) {
-          expect(res).to.have.status(405);
-          done();
-        });
+    it('POST should not be supported.', () => {
+      return (async () => {
+        try {
+          await chai.request(Url.Query).post('/');
+        } catch (err) {
+          expect(err).to.have.status(405);
+        }
+      })();
     });
 
   });
